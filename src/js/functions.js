74b560b168618c54b0a25ae294f4b8148d2a6891
giodum <giodum@gmail.com>
2018-01-17 09:14:08 +0000

    Clustering structure

--- src/js/functions.js
+++
@@ -1,8 +0,0 @@
-function getRGB(str){
-  var match = str.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
-  return match ? {
-    red: match[1],
-    green: match[2],
-    blue: match[3]
-  } : {};
-}
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,20 @@
 var clusterMaker = require('clusters');
+var Color = require('color');
 
 $(function() {
 
-  console.log("Start main js");
-
   var nItems = 150;
+  var nIterations = 150;
   var nClusters = 5;
   var items = [];
 
-  var container = $('.container');
+  var containerUnclustered = $('.container-unclustered');
+  var containerClustered = $('.container-clustered');
 
   /* generate colored circles */
+
+  console.log("START GENERATING AND RENDERING RANDOM COLORS");
+
   for(var i = 0; i < nItems; i++) {
 
     var selector = 'item-' + i;
@@ -19,18 +23,49 @@ $(function() {
        format: 'rgb'
     });
 
-    container.append('<div class="item ' + selector + '"></div>');
+    containerUnclustered.append('<div class="item ' + selector + '"></div>');
     $('.' + selector).css('background-color', color);
 
-    items[i] = color;
+    items[i] = Color(color).rgb().array();
+
+    console.log("  ", selector, " - ", items[i]);
   }
 
+  console.log("END GENERATING RANDOM COLORS\n");
+
+
   /* initialization of K-means parameter */
+
+  console.log("START INITIALIZING K-MEANS PARAMETERS\n");
+
   clusterMaker.k(nClusters);
-  clusterMaker.iterations(nItems);
+  clusterMaker.iterations(nIterations);
+
+  console.log("  - Number of clusters: ", nClusters);
+  console.log("  - Number of iterations: ", nIterations);
+  console.log("  - Number of items to be clustered: ", nItems);
+
+  clusterMaker.data(items);
 
 
+  console.log("START CLUSTERING...")
 
-  console.log("End main js");
+  var startClustering = new Date().getTime();
+  var clusters = clusterMaker.clusters();
+  var endClustering = new Date().getTime();
+
+  console.log("...END CLUSTERING - Total duration: ", endClustering - startClustering, "ms");
+
+
+  console.log("START RENDERING CLUSTERS");
+
+  /* generate clusters containers */
+  for(var i = 0; i < nClusters; i++) {
+
+    var selector = 'cluster-' + i;
+    containerClustered.append('<div class="cluster ' + selector + '"></div>');
+  }
+
+  console.log("END RENDERING CLUSTERS");
 
 });
